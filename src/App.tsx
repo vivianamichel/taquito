9a8dd5f1298489967847c866397415ab0dd43fa6
Viviana Michel <vmichel@berkeley.edu>
2021-04-25 09:16:58 -0700

    wallet

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { TezosToolkit } from "@taquito/taquito";
 import "./App.css";
 import ConnectButton from "./components/ConnectWallet";
 import DisconnectButton from "./components/DisconnectWallet";
-import qrcode from "qrcode-generator";
-import UpdateContract from "./components/UpdateContract";
 import Transfers from "./components/Transfers";
 
 enum BeaconConnection {
@@ -29,32 +27,18 @@ const App = () => {
   const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useState<string>("transfer");
 
-  // Florencenet Increment/Decrement contract
   const contractAddress: string = "KT1XCAdyQsHuNTkeLJes7R3krmGhtsmoBvNz";
-
-  const generateQrCode = (): { __html: string } => {
-    const qr = qrcode(0, "L");
-    qr.addData(publicToken || "");
-    qr.make();
-
-    return { __html: qr.createImgTag(4) };
-  };
 
   if (publicToken && (!userAddress || isNaN(userBalance))) {
     return (
       <div className="main-box">
-        <h1>Taquito Boilerplate</h1>
+        <h1>Brandnetic</h1>
         <div id="dialog">
-          <header>Try the Taquito Boilerplate App!</header>
           <div id="content">
             <p className="text-align-center">
               <i className="fas fa-broadcast-tower"></i>&nbsp; Connecting to
               your wallet
             </p>
-            <div
-              dangerouslySetInnerHTML={generateQrCode()}
-              className="text-align-center"
-            ></div>
             <p id="public-token">
               {copiedPublicToken ? (
                 <span id="public-token-copy__copied">
@@ -84,66 +68,26 @@ const App = () => {
             </p>
           </div>
         </div>
-        <div id="footer">
-          <img src="built-with-taquito.png" alt="Built with Taquito" />
-        </div>
       </div>
     );
   } else if (userAddress && !isNaN(userBalance)) {
     return (
       <div className="main-box">
-        <h1>Taquito Boilerplate</h1>
         <div id="tabs">
-          <div
-            id="transfer"
-            className={activeTab === "transfer" ? "active" : ""}
-            onClick={() => setActiveTab("transfer")}
-          >
-            Make a transfer
-          </div>
-          <div
-            id="contract"
-            className={activeTab === "contract" ? "active" : ""}
-            onClick={() => setActiveTab("contract")}
-          >
-            Interact with a contract
+          <div>
+            Contract
           </div>
         </div>
         <div id="dialog">
           <div id="content">
-            {activeTab === "transfer" ? (
               <div id="transfers">
-                <h3 className="text-align-center">Make a transfer</h3>
+                <h3 className="text-align-center">Contract Amount</h3>
                 <Transfers
                   Tezos={Tezos}
                   setUserBalance={setUserBalance}
                   userAddress={userAddress}
                 />
               </div>
-            ) : (
-              <div id="increment-decrement">
-                <h3 className="text-align-center">
-                  Current counter: <span>{storage}</span>
-                </h3>
-                <UpdateContract
-                  contract={contract}
-                  setUserBalance={setUserBalance}
-                  Tezos={Tezos}
-                  userAddress={userAddress}
-                  setStorage={setStorage}
-                />
-              </div>
-            )}
-            <p>
-              <i className="far fa-file-code"></i>&nbsp;
-              <a
-                href={`https://better-call.dev/florencenet/${contractAddress}/operations`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {contractAddress}
-              </a>
-            </p>
             <p>
               <i className="far fa-address-card"></i>&nbsp; {userAddress}
             </p>
@@ -162,43 +106,12 @@ const App = () => {
             setBeaconConnection={setBeaconConnection}
           />
         </div>
-        <div id="footer">
-          <img src="built-with-taquito.png" alt="Built with Taquito" />
-        </div>
       </div>
     );
   } else if (!publicToken && !userAddress && !userBalance) {
     return (
       <div className="main-box">
-        <div className="title">
-          <h1>Taquito Boilerplate</h1>
-          <a href="https://app.netlify.com/start/deploy?repository=https://github.com/ecadlabs/taquito-boilerplate">
-            <img
-              src="https://www.netlify.com/img/deploy/button.svg"
-              alt="netlify-button"
-            />
-          </a>
-        </div>
         <div id="dialog">
-          <header>Welcome to Taquito Boilerplate App!</header>
-          <div id="content">
-            <p>Hello!</p>
-            <p>
-              This is a template Tezos dApp built using Taquito. It's a starting
-              point for you to hack on and build your own dApp for Tezos.
-              <br />
-              If you have not done so already, go to the{" "}
-              <a
-                href="https://github.com/ecadlabs/taquito-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Taquito boilerplate Github page
-              </a>{" "}
-              and click the <em>"Use this template"</em> button.
-            </p>
-            <p>Go forth and Tezos!</p>
-          </div>
           <ConnectButton
             Tezos={Tezos}
             setContract={setContract}
@@ -211,9 +124,6 @@ const App = () => {
             setBeaconConnection={setBeaconConnection}
             wallet={wallet}
           />
-        </div>
-        <div id="footer">
-          <img src="built-with-taquito.png" alt="Built with Taquito" />
         </div>
       </div>
     );
--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -86,7 +86,7 @@ const ConnectButton = ({
     (async () => {
       // creates a wallet instance
       const wallet = new BeaconWallet({
-        name: "Taquito Boilerplate",
+        name: "BRANDNETIC",
         preferredNetwork: NetworkType.EDONET,
         disableDefaultEvents: true, // Disable all events / UI. This also disables the pairing alert.
         eventHandlers: {
